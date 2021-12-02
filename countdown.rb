d64ba133eaafdd38bed9fe99737d0a0e48bd68bc
Ray's Git <lancelot53.ra@gmail.com>
2021-12-02 14:54:22 -0500

    Completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,31 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    counter = 1
+    while number >= counter do
+        puts "#{number} SECOND(S)!\n"
+        number-=1
+    sleep 1
+     #countdown_with_sleep
+
+    end
+     return "HAPPY NEW YEAR!"
 end
+countdown(10)
+puts "\n"
+def countdown_with_sleep(number)
+    counter = 1
+    while number >= counter do
+        puts "#{number} SECOND(S)!\n"
+        number-=1
+    sleep 1
+     #countdown_with_sleep
+
+    end
+     return "HAPPY NEW YEAR!"
+     return number + 10
+    #sleep 5
+   end
+   puts countdown_with_sleep(6)
+   countdown_with_sleep(10)
+
